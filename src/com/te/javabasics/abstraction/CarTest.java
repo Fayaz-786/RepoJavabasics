842049cae0f8d4f718f73fefcdb27864e1b0c569
Fayaz-786 <113663957+Fayaz-786@users.noreply.github.com>
2022-10-10 12:30:05 +0530

    Delete src/com/te/javabasics/abstraction directory

--- src/com/te/javabasics/abstraction/CarTest.java
+++
@@ -1,9 +0,0 @@
-package com.te.javabasics.abstraction;
-
-public class CarTest {
-	public static void main(String[] args) {
-		Car car = CarFactory.getCar();
-		Driver driver=new Driver();
-       driver.driver(car);       
-}
-}
